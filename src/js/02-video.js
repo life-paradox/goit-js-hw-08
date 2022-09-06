b39a0044e65a6bb031d20568368f5f290c252ccc
life-paradox <zhideevpavel@gmail.com>
2022-09-06 10:19:25 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,14 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
-// var throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const playerTime = localStorage.getItem('videoplayer-current-time');
 
 window.addEventListener('DOMContentLoaded', () => {
-  player.setCurrentTime(playerTime);
+  if (playerTime) {
+    player.setCurrentTime(playerTime);
+  }
 });
 
 player.on(
